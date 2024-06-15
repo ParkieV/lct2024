d1399d151aea864b829b0fd3ed860abc317d00fa
domster704 <domster704@mail.ru>
2024-06-15 20:38:24 +0300

    start make adaptive

--- src/components/pages/EmployeesPage/EmployeesList/Row/Row.jsx
+++ src/components/pages/EmployeesPage/EmployeesList/Row/Row.jsx
@@ -7,6 +7,19 @@ import deleteSvg from './img/delete.svg';
 import EmployeeEdit from "../../EmployeeEdit/EmployeeEdit";
 import {deleteEmployee} from "../../../../../store/employeeSlice";
 
+const ToolsButton = ({onClickEdit, onClickDelete, isEdit}) => {
+    return (
+        <>
+            <button onClick={onClickEdit}>
+                <img src={isEdit ? saveSvg : editSvg} alt=""/>
+            </button>
+            <button onClick={onClickDelete}>
+                <img src={deleteSvg} alt=""/>
+            </button>
+        </>
+    )
+}
+
 const Row = ({
                  index,
                  employee
@@ -28,18 +41,39 @@ const Row = ({
     return (
         <>
             <div className={style.row}>
-                <b>{employee.index}</b>
-                <b>{employee.last_name} {employee.first_name[0]}.{employee.middle_name[0]}</b>
-                <p className={style.position}>{employee.position}</p>
-                <p className={style.organization}>{organizations[employee.work_org_id].label}</p>
-                <p className={style.type}>{types[employee.type]}</p>
+                <div className={style.header_mobile}>
+                    <div className={style.with_header}>
+                        <p className={style.mobile_header_text}>ID</p>
+                        <b className={style.id}>{employee.index}</b>
+                    </div>
+
+                    <div className={`${style.actions} ${style.actions_mobile}`}>
+                        <ToolsButton onClickEdit={onClickEdit}
+                                     onClickDelete={onClickDelete}
+                                     isEdit={isEdit}/>
+                    </div>
+                </div>
+                <div className={style.with_header}>
+                    <p className={style.mobile_header_text}>ФИО</p>
+                    <b>{employee.last_name} {employee.first_name[0]}.{employee.middle_name[0]}</b>
+                </div>
+                <div className={style.with_header}>
+                    <p className={style.mobile_header_text}>Должность</p>
+                    <p className={style.position}>{employee.position}</p>
+                </div>
+                <div className={style.with_header}>
+                    <p className={style.mobile_header_text}>ID</p>
+                    <p className={style.organization}>{organizations[employee.work_org_id].label}</p>
+                </div>
+                <div className={style.with_header}>
+                    <p className={style.mobile_header_text}>ID</p>
+                    <p className={style.type}>{types[employee.type]}</p>
+                </div>
+
                 <div className={style.actions}>
-                    <button onClick={onClickEdit}>
-                        <img src={isEdit ? saveSvg : editSvg} alt=""/>
-                    </button>
-                    <button onClick={onClickDelete}>
-                        <img src={deleteSvg} alt=""/>
-                    </button>
+                    <ToolsButton onClickEdit={onClickEdit}
+                                 onClickDelete={onClickDelete}
+                                 isEdit={isEdit}/>
                 </div>
             </div>
             {
