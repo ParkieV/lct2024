a1a14681b8766e7871bb428829ae7f1bca61716f
Avenir <kadomcev2004@gmail.com>
2024-06-16 19:07:30 +0300

    Fix plots. Change return type to excel router

--- ml/api/src/routers/v1/analytics_all_router.py
+++ ml/api/src/routers/v1/analytics_all_router.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+import base64
+
 from fastapi import APIRouter, HTTPException, Depends, Query
 import pandas as pd
 from tempfile import NamedTemporaryFile
@@ -5,7 +8,7 @@ from fastapi.responses import FileResponse
 from matplotlib import pyplot as plt
 
 from api.src.configurations.users import get_user_session
-from api.src.schemas.schemas import LeftoverSchema, PurchasesSchema, DebitCreditSchema
+from api.src.schemas.schemas import LeftoverSchema, PurchasesSchema,  ExcelSchema
 from api.src.services.analytics_service import get_history, get_purchases
 
 analytics_all_router = APIRouter(
@@ -16,18 +19,17 @@ analytics_all_router = APIRouter(
 plt.switch_backend('Agg')
 
 
-@analytics_all_router.get("/history")
+@analytics_all_router.get("/history", response_model=ExcelSchema)
 def get_last_n_history(n: int):
     df = get_history().head(n)
 
-    # Save the DataFrame to a temporary Excel file
-    with NamedTemporaryFile(delete=False, suffix='.xlsx') as tmp:
-        with pd.ExcelWriter(tmp.name, engine='xlsxwriter') as writer:
-            df.to_excel(writer, index=False)
-    
-        # Return the file as a download
-        return FileResponse(path=tmp.name, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename=f"history_head_{n}.xlsx")
-    
+    buf = BytesIO()
+    df.to_excel(buf, index=False)
+    buf.seek(0)
+
+    excel_file = base64.b64encode(buf.read()).decode('utf-8')
+    return {'file': excel_file}
+
 
 @analytics_all_router.get("/purchase_stats", response_model=PurchasesSchema)
 def get_purchase_stats(period: int, summa: bool):
