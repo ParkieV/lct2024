e0ed7536d2eafc5c39c09a627787cb433755fa0e
domster704 <domster704@mail.ru>
2024-06-16 13:32:07 +0300

    fix all v2

--- backend/src/app/persistence/repositories/sql_error_handler.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.exc import IntegrityError
-
-
-def sql_validation_error(exception) -> str:
-    if isinstance(exception, IntegrityError):
-        str_exc = exception.orig.args[0] # type: ignore
-        exc_message = str_exc[str_exc.find(': ')+2:]
-        return exc_message
-    return "Unexpected error"
