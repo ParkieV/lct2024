08b3e29fc236bb13043c83db7c9dc6ed19cb8d22
domster704 <domster704@mail.ru>
2024-06-16 21:09:19 +0300

    move to folder

--- res/choose_purchase_text.py
+++
@@ -1,9 +0,0 @@
-ACTIVE_PURCHASE_HELLO_TEXT = f"""Выберите закупку из списка путем ввода номера из списка"""
-CHOOSE_PURCHASE_TEXT = lambda purchase: f"""Вы выбрали {purchase}"""
-
-EDIT_PURCHASE_BUTTON_TEXT = "Товары"
-DELETE_PURCHASE_BUTTON_TEXT = "Удалить закупку"
-DOWNLOAD_PURCHASE_BUTTON_TEXT = "Скачать закупку"
-
-DELETE_PURCHASE_SUCCESS_MESSAGE = "Закупка удалена"
-NO_PURCHASES_TEXT = "Нет закупок для выбора"
